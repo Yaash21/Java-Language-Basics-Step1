7e05c07a17a651b67b103756d9db46e1076a5e1a
Yaash21 <yaashjain21212@gmail.com>
2018-02-15 10:35:03 +0530

    Practice Excersices

--- src/org/stackroute2/Date.java
+++ src/org/stackroute2/Date.java
@@ -1,5 +1,18 @@
 package org.stackroute2;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Date {
 
+	public static void main(String[] args) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEE dd/MM/yyyy");
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		System.out.println(dateFormat.format(calendar.getTime()));
+		for (int i = 0; i < 6; i++) {
+			calendar.add(Calendar.DATE, 1);
+		}
+		System.out.println(dateFormat.format(calendar.getTime()));
+	}
 }
